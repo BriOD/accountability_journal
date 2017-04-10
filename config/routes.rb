28a21d3414346a7b960a883b458a0085f9d6aa48
BriOD <briodonoghue@gmail.com>
2017-04-10 17:40:01 -0400

    added user and session controller. mapped out routes for signup and login

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # user login and registration
+  root 'session#login'
+
+  get '/signup', to: "users#signup", as: 'user_signup'
+  post '/users', to: "users#create", as: 'create_user'
+  get '/login', to: "sessions#new", as: 'login'
+  post '/login', to: "sessions#create", as: "create_session"
+  delete '/logout', to: "sessions#destroy", as: "logout"
 end
