94198f57d335fee759ae6cd257119d5a06116893
BriOD <briodonoghue@gmail.com>
2017-04-10 16:59:37 -0400

    changed Journal date type from :date to :string

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170410194525) do
+ActiveRecord::Schema.define(version: 20170410205149) do
 
   create_table "activity_accounts", force: :cascade do |t|
     t.string   "daily_activity"
@@ -33,7 +33,7 @@ ActiveRecord::Schema.define(version: 20170410194525) do
   end
 
   create_table "entries", force: :cascade do |t|
-    t.date     "date"
+    t.string   "date"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer  "journal_id"
