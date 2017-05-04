db941cd9d8f50304ae62c3f12870ac195a276016
BriOD <briodonoghue@gmail.com>
2017-05-04 00:16:25 -0400

    started building low level hqerry for entry show page

--- app/assets/javascripts/entries.js
+++ app/assets/javascripts/entries.js
@@ -1,6 +1,18 @@
 
 $(function(){
-  $("#new_activity_account").on("submit", function(){
-    alert("you clicked submit")
+  $("#new_activity_account").on("submit", function(e){
+    console.log(this)
+    // 1. need a URL to submit the POST request to
+    url = this.action
+
+    data = {
+      'auth_token' : $("input[name='authenticity_token']").value(),
+      'activity_account' : {
+        
+      }
+    }
+    // 2. need form data
+
+    e.preventDefault();
   })
 })
